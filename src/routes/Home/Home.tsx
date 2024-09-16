9451102fe1bacb33e81ea06540672094dac53786
ntncaue <caueraujo60@gmail.com>
2024-09-16 16:06:51 -0300

    Section Hero desenvolvida de estilizada

--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -1,7 +1,22 @@
+import React from 'react';
+import styles from '../../style/Home.module.css'
+
+
 const Home: React.FC = () => {
-    return (
-    <h1>Home</h1>
-    );
-    }
-    
+  return (
+    <main className={styles.home}>
+      <section className={styles.heroSection}>
+        <div className={styles.heroContent}>
+          <h2>Diagnóstico, <br /> Rápido e  <br /> Preciso.</h2>
+          <p>Consulte nossos serviços online de forma fácil e rápida.<br />
+            Identifique problemas no seu veículo em poucos cliques e <br />
+            mantenha tudo sob controle com eficiência.</p>
+          <button className={styles.discoverButton}>Descubra</button>
+        </div>
+      </section>
+    </main>
+
+ 
+  );
+}
 export default Home;

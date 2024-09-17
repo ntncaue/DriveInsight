18f942fd8b311b23e67bcf23b1eebcd096b2f30c
ntncaue <caueraujo60@gmail.com>
2024-09-16 21:55:34 -0300

    Correção de estilização e lógica do toggleMenu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import styles from './header.module.css';
 import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
-import styles from '../../style/Header.module.css';
+import { Link } from 'react-router-dom';
 
 const Header: React.FC = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -10,17 +10,21 @@ const Header: React.FC = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  const closeMenu = () => {
+    setIsMenuOpen(false); 
+  };
+
   return (
     <header className={styles.header}>
       <div className={styles.container}>
         <h1 className={styles.logo}>DRIVEINSIGHT</h1>
         <nav>
           <ul className={`${styles.navList} ${isMenuOpen ? styles.active : ''}`}>
-            <li><Link to="/" className={styles.navLink}>Home</Link></li>
-            <li><a href="#sobre-nos" className={styles.navLink}>Sobre Nós</a></li>
-            <li><a href="#servicos" className={styles.navLink}>Serviços</a></li>
-            <li><a href="#equipe" className={styles.navLink}>Equipe</a></li>
-            <li><a href="#contato" className={styles.navLink}>Contato</a></li>
+          <li><Link to="/" className={styles.navLink}>Home</Link></li>
+            <li><a href="#sobre-nos" className={styles.navLink} onClick={closeMenu}>Sobre Nós</a></li>
+            <li><a href="#servicos" className={styles.navLink} onClick={closeMenu}>Serviços</a></li>
+            <li><a href="#equipe" className={styles.navLink} onClick={closeMenu}>Equipe</a></li>
+            <li><a href="#contato" className={styles.navLink} onClick={closeMenu}>Contato</a></li>
           </ul>
         </nav>
         <button className={styles.ctaButton}>Contato</button>
